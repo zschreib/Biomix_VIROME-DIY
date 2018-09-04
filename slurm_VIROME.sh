b205f5ab405d784951c029bc03e71300a63740de
Zach Schreiber <zschreib@udel.edu>
2018-09-04 17:42:08 -0400

    init

--- slurm_VIROME.sh
+++ slurm_VIROME.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH --partition=docker --account=docker
-#SBATCH --ntasks=
-#SBATCH --mem=
-#SBATCH -J 
+#SBATCH --ntasks=6
+#SBATCH --mem=12000
+#SBATCH -J test
 
-perl execute_pipeline.pl -i= -o= -d= --threads=
+perl execute_pipeline.pl -i=/input.fa -o=/output -d=/database --threads=6
